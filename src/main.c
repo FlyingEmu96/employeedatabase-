8747a7a781ac8389657a592b1df037a8b8daee4b
Emir_Flying_Emu96 <emir.murat1337@gmail.com>
2025-08-24 12:08:47 +0100

    Refactor print_usage function and clean main logic

--- src/main.c
+++ src/main.c
@@ -1,74 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <unistd.h>    // getopt portability
 #include <getopt.h>
 #include <stdlib.h>
+
 #include "common.h"
 #include "file.h"
 #include "parse.h"
 
-void print_usage(const char *prog) {
-    printf("Usage: %s -n -f <database file>\n", prog);
-    printf("\t -n - create new database file\n");
-    printf("\t -f - (required) path to database\n");
+void print_usage(char *argv[]) {
+    printf("Usage: %s -n -f <database file>\n", argv[0]);
+    printf("\t -n -create new database file\n");
+    printf("\t -f (required) path to database file\n");
+    return;
 }
 
 int main(int argc, char *argv[]) {
+
     char *filepath = NULL;
     bool newfile = false;
     int c;
 
-    int dbfd = -1;
-    struct dbhdr_t *dbhdr = NULL;
-
-    while ((c = getopt(argc, argv, "nf:")) != -1) {
+    while ((c = getopt(argc, argv, "nf:")) != -1){
         switch (c) {
             case 'n':
-                newfile = true;
-                break;
+                    newfile = true;
+                    break;
             case 'f':
-                filepath = optarg;
-                break;
+                    filepath = optarg;
+                    break;
             case '?':
-                printf("Unknown option -%c\n", optopt ? optopt : '?');
-                print_usage(argv[0]);
-                return 2;
+                    printf("Uknown option -%c\n", c);
+
+                    break;
             default:
-                return 2;
+                    return -1;
+
         }
     }
-
-    if (filepath == NULL) {
+    
+    if (filepath == NULL){
         printf("Filepath is a required argument\n");
-        print_usage(argv[0]);
-        return 2;
+        print_usage(argv);
+
+        return 0;
     }
+    
+    printf("Newfile: %d\n", newfile);
+    printf("Filepath %s\n", filepath);
 
-    if (newfile) {
-        dbfd = create_db_file(filepath);
-        if (dbfd == STATUS_ERROR) {              // or: if (dbfd < 0)
-            printf("Unable to open database file\n");
-            return -1;
-        }
-        if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
-            printf("Failed to create database header\n");
-            close(dbfd);
-            return -1;
-        }
-    } else {
-        dbfd = open_db_file(filepath);
-        if (dbfd == STATUS_ERROR) {              // or: if (dbfd < 0)
-            printf("Unable to open database file\n");
-            return -1;
-        }
-        // TODO: read existing header here
-    }
-
-    printf("Newfile: %d\n", newfile ? 1 : 0);
-    printf("filepath: %s\n", filepath);
-
-    // cleanup (add before any early returns where needed)
-    if (dbhdr) { free(dbhdr); dbhdr = NULL; }
-    if (dbfd >= 0) close(dbfd);
-    return 0;
+	return 0;
 }
