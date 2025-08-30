9d5a3c674b280dbfdc41c699a3f10f02971800cf
Emir_Flying_Emu96 <emir.murat1337@gmail.com>
2025-08-30 21:19:23 +0100

    Refactor parse.h for clarity and function updates

--- include/parse.h
+++ include/parse.h
@@ -1,15 +1,15 @@
 #ifndef PARSE_H
 #define PARSE_H
 
-#include <stddef.h>  // for NULL if needed in some environments
+#include <stddef.h>
 
 #define HEADER_MAGIC 0x4c4c4144
 
 struct dbheader_t {
-    unsigned int magic;
+    unsigned int   magic;
     unsigned short version;
     unsigned short count;
-    unsigned int filesize;
+    unsigned int   filesize;
 };
 
 struct employee_t {
@@ -19,11 +19,21 @@ struct employee_t {
 };
 
 int create_db_header(struct dbheader_t **headerOut);
-
 int validate_db_header(int fd, struct dbheader_t **headerOut);
 
-int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut);
+int add_employee(struct dbheader_t *dbhdr,
+                 struct employee_t **employeesOut,
+                 char *addstring);
 
-int output_file(int fd, struct dbheader_t *header, void *unused);
+int read_employees(int fd,
+                   struct dbheader_t *dbhdr,
+                   struct employee_t **employeesOut);
 
-#endif // PARSE_H
+int output_file(int fd,
+                struct dbheader_t *header,
+                void *unused);
+
+void list_employees(struct dbheader_t *dbhdr,
+                    struct employee_t *employees);
+
+#endif
